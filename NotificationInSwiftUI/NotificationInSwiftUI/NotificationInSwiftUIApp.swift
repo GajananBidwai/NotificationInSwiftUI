bba29e761aeee980a2431376ab688458abd629fb
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-12-16 15:43:21 +0530

    Initial commit

---
+++ NotificationInSwiftUI/NotificationInSwiftUI/NotificationInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationInSwiftUIApp.swift
+//  NotificationInSwiftUI
+//
+//  Created by Neosoft on 16/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NotificationInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
