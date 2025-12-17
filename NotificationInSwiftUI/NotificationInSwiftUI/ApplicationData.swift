e15611beb1d90c7ebe06d4db561f997cca85f55f
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-12-17 10:58:36 +0530

    Implemented custom sound and thumbnail to the local notification

--- NotificationInSwiftUI/NotificationInSwiftUI/ApplicationData.swift
+++ NotificationInSwiftUI/NotificationInSwiftUI/ApplicationData.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Observation
 import UserNotifications
+import UIKit
 
 @Observable class ApplicationData: @unchecked Sendable {
     @ObservationIgnored let center = UNUserNotificationCenter.current()
@@ -31,6 +32,13 @@ import UserNotifications
             let content = UNMutableNotificationContent()
             content.title = "Reminder"
             content.body = messeage
+            content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "sample-3s.mp3"))
+            let idImage = "attach\(UUID())"
+            if let urlImage = await getThumbNail(id: idImage) {
+                if let attachment = try? UNNotificationAttachment(identifier: idImage, url: urlImage, options: nil) {
+                    content.attachments = [attachment]
+                }
+            }
             
             let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
             let id = "reminder-\(UUID())"
@@ -44,6 +52,24 @@ import UserNotifications
             
         }
     }
+    
+    func getThumbNail(id: String) async ->  URL?{
+        let manager = FileManager.default
+        if let docUrl = manager.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let fileUrl = docUrl.appendingPathComponent("\(id).png") as URL?
+            if let image = UIImage(named: "sample") {
+                if let thumbnail = await image.preparingThumbnail(of: CGSize(width: 100, height: 100)) {
+                    if let imageData = thumbnail.pngData() {
+                        if let _ = try? imageData.write(to: fileUrl!) {
+                            return fileUrl
+                        }
+                    }
+                }
+            }
+        }
+        return nil
+    }
+    
     
     
 }
